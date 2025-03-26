f512788f8eb6e84f255e6485948254f712891e5b
T9Air <163940743+T9Air@users.noreply.github.com>
2025-03-25 22:34:50 -0400

    Create control.py
    Initial code: does not include code for calculating angles (law of cosines) or sending and receiving data. Will include y axis after adding that servo motor to the arm.
    Will need to update code eventually to match a config file for the arm to make it easily configurable with different arms.

---
+++ control.py
@@ -0,0 +1,49 @@
+import serial
+
+#################################################################
+# 1. Add code for calculating angles of the joints
+# 2. Add code for sending angles to the arduino
+# 3. Add code for receiving data from the arduino
+# 4. Add code for y axis after adding rotation along z axis
+#################################################################
+
+def get_xyz_input(prev_x, prev_z):
+    print("Press 'q' to quit, and 's' to keep the axis in the same position")
+    x = input("Enter the x coordinate: ")
+    if x == 'q':
+        return False
+    elif x == 's':
+        x = prev_x
+    # y = input("Enter the y coordinate: ")
+    # if y == 'q':
+    #     return False
+    # elif y == 's':
+    #     y = prev_y
+    z = input("Enter the z coordinate: ")
+    if z == 'q':
+        return False
+    elif z == 's':
+        z = prev_z
+    return x, z
+
+def calculate_angles(x, z):
+    # calculate the angles of the joints
+    return 
+
+def main():
+    ser = serial.Serial('COM3', 9600)
+    
+    x = 0 # initial x coordinate: change to actual initial position
+    #y = 0 # initial y coordinate: change to actual initial position
+    z = 0 # initial z coordinate: change to actual initial position
+
+    #joint_1 = 90 # rotate on z axis
+    joint_2 = 90 # base joint
+    joint_3 = 90 # first arm joint
+    
+    while True:
+        positions = get_xyz_input(x, z)
+        if positions == False:
+            break
+        x, z = positions
+        joint_2, joint_3 = calculate_angles(x,z)
